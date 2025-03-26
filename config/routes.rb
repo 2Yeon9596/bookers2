bd492df72c4cabb466dba0b2267bffd3b6b5b17c
출구없정연이의입구 <jeongyeononly1101@chulgueobsjeong-yeon-iuiibgunoMacBook-Air.local>
2025-03-26 21:37:09 +0900

    routes, model編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  resources :books, only: [:index, :show, :edit, :update, destroy]
+  resources :users, only: [:index, :show, :edit, :update]
   root to: "homes#top"
   get "about" => "homes#about"
 end
