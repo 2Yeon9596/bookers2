55504f2cf27c81973e39da546739f1dd576b59ee
출구없정연이의입구 <jeongyeononly1101@chulgueobsjeong-yeon-iuiibgunoMacBook-Air.local>
2025-03-26 21:19:06 +0900

    model, routes, controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do 
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  root to: "homes#top"
+  get "about" => "homes#about"
 end
