6080f93617414d590d23f5cb883b095546c915ab
출구없정연이의입구 <jeongyeononly1101@chulgueobsjeong-yeon-iuiibgunoMacBook-Air.local>
2025-03-28 21:46:46 +0900

    バリデーション作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,14 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to books_path
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to books_path
+    else
+      @books = Book.all
+      @user = current_user
+      render :index, status: :unprocessable_entity
+    end    
   end   
 
   def show
@@ -25,9 +30,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(@book)
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
+    else
+      render :edit, status: :unprocessable_entity
+    end  
   end  
 
   def destroy
